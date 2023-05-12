f12409f8c49a0f95f5024528d131764bcd0dd8a5
JohanaMartinezWeb <johanagaray2314@gmail.com>
2023-05-12 17:25:22 -0300

    Todos los archivos de OpenWeather

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+const NavBar = () => {
+	return (
+		<nav className="navbar bg-dark text-light mb-5" >
+			<div className="container-fluid">
+				<h3 className="mx-auto">El Tiempo es Ahora</h3>
+			</div>
+		</nav>
+	);
+}
+export default NavBar;
